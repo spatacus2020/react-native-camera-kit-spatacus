6b9ac9e89e35917cf964c0043b7c19767a6f02d3
lijunfeng <364756334@qq.com>
2020-09-10 10:56:05 +0800

    first commit

---
+++ src/CameraScreen/CameraKitCameraScreenStyleObject.ios.js
@@ -0,0 +1,15 @@
+export default  {
+  bottomButtons: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 14
+  },
+//   bottomContainerGap: {
+//     flex: 1,
+//     flexDirection: 'row',
+//     justifyContent: 'flex-end',
+//     alignItems: 'center',
+//     padding: 10
+//   }
+}
